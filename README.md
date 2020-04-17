a2b45b844566438a40e80f6a60aa0d4b4a2d5e3f
NoblesseCoder <ashobot@gmail.com>
2020-04-17 13:14:22 -0400

    update README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RL-Agents
-Implementation of various Reinforcement Learning Agents
+The repo contains implementations of various Reinforcement Learning Agents.
+
+
+### Basic Agents
+
+1. [Random agent (default)](./tabular/agent.py)
+2. [Human agent](./tabular/human_agent.py)
+3. [Basic ε-greedy agent](./tabular/epsilon_greedy_agent.py)
+
+### References
+
+[1] Reinforcement Learning: An Introduction, Richard S. Sutton & Andrew G. Barto (second edition)
--- tabular/epsilon_greedy_agent.py
+++ tabular/epsilon_greedy_agent.py
@@ -13,18 +13,12 @@ class EpsilonGreedyAgent(Agent):
 		self.action_history = []
 		self.rewards_history = []
 
-	def is_exploit(self):
-		# Return 1 if exploit, else return 0
-		choices = [0,1]
-		probs = [self.epsilon, 1-self.epsilon]
-		return(np.random.choice(choices, 1, p=probs))
-	
 	def explore_act(self):
 		# Exploration (non-greedy): choose action randomly from the action space
 		action = Agent.act()
 		return action
 
-	def action_val_estimation_SAM(self, action, time_step):
+	def _action_val_estimation_SAM(self, action, time_step):
 		# Action value estimation via Sample Average Method (SAM)
 		numerator, denominator = 0, 0
 		for t in range(time_step):
@@ -40,16 +34,16 @@ class EpsilonGreedyAgent(Agent):
 		# Exploitation (greedy): choose action with max estimated action value 
 		estimated_action_vals = []
 		for action in self.action_space:
-			estimated_action_vals.append(self.estimate_action_val(action,time_step))
+			estimated_action_vals.append(self._action_val_estimation_SAM(action,time_step))
 		action_ind = np.argmax(np.array(estimated_action_vals))
 		return(self.action_space[action_ind])	
 
 	def act(self, time_step, reward, done):
 		# Return chosen action 
 		action_type = self.is_exploit()
-		if (action_type == 0):
+		if(np.random.rand() < self.epsilon):
 			action = self.explore_act()
-		if (action_type == 1):
+		else:
 			action = self.exploit_act(time_step)
 		self.action_history.append(action)
 		self.rewards_history.append(reward)
