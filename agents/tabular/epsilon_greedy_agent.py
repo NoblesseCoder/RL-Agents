6e7ac41bf17ab6568f0cc2403ed708b33e5a3b1f
NoblesseCoder <ashobot@gmail.com>
2020-04-19 01:16:08 -0400

    modify epsilon greed agent update method from SAM to Incremental update

--- agents/tabular/epsilon_greedy_agent.py
+++ agents/tabular/epsilon_greedy_agent.py
@@ -10,8 +10,8 @@ class EpsilonGreedyAgent(Agent):
 	def __init__(self, action_space, n_actions, epsilon=0.1):
 		self.n_actions = n_actions
 		self.epsilon = epsilon
-		self.reward_history = []
-		self.action_history = []
+		self.Q_actions = [0 for i in range(n_actions)] # Q(a) value estimates for all actions
+		self.N_actions = [0 for i in range(n_actions)] # N(a) for all actions
 		Agent.__init__(self, action_space)
 
 	def explore_act(self, state, reward, done):
@@ -19,31 +19,16 @@ class EpsilonGreedyAgent(Agent):
 		action = Agent.act(self,state, reward, done)
 		return action
 
-	def _action_val_estimation_SAM(self, action, time_step):
-		# Action value estimation via Sample Average Method (SAM)
-
-		numerator = np.sum([self.reward_history[i] for i in range(len(self.reward_history)) if self.action_history[i]==action])
-		denominator = np.sum([self.action_history[i]==action for i in range(len(self.action_history))])
-
-		if (denominator == 0):
-			return 0	
-		return(numerator/denominator) 
-
-	def exploit_act(self, time_step):
+	def exploit_act(self):
 		# Exploitation (greedy): choose action with max estimated action value 
-		estimated_action_vals = []
-		for action in range(self.n_actions):
-			estimated_action_vals.append(self._action_val_estimation_SAM(action,time_step))
-		action_ind = np.argmax(np.array(estimated_action_vals))
-		return(action_ind)	
+		return(np.argmax(self.Q_actions))	
 
-	def act(self, state, time_step, reward, done):
+	def act(self, state, reward, done):
 		# Return chosen action 
 		if(np.random.rand() < self.epsilon):
 			action = self.explore_act(state, reward, done)
 		else:
-			action = self.exploit_act(time_step)
-		self.reward_history.append(reward)
-		self.action_history.append(action)
-		#print(self.action_history)
+			action = self.exploit_act()
+		self.N_actions[action] += 1
+		self.Q_actions[action] += (reward - self.Q_actions[action])/self.N_actions[action]  
 		return action
--- demos/epsilon_greedy_agent_demo.py
+++ demos/epsilon_greedy_agent_demo.py
@@ -24,28 +24,51 @@ def train_epsilon_greedy_agent(episodes = 100, epsilon=0.1):
 	prev_state = env.reset()
 	done = False
 
-	average_rewards = []
+	total_rewards = [0]
+	average_rewards = [0]
+	init_reward = 0
 	for episode in range(episodes):
 		while True:
-			next_state, reward, done, _ = env.step(agent.act(prev_state, episode, reward, done))
+			next_state, reward, done, _ = env.step(agent.act(prev_state, reward, done))
 			print("Episode:" + str(episode) + " Reward:" + str(reward))
 			if done:
 				break
-		average_rewards.append(np.mean(agent.reward_history))
+		init_reward += reward
+		total_rewards.append(init_reward)
+		average_rewards.append(init_reward/(episode+1))
 	env.close()
-	return(average_rewards)
+	return(average_rewards,total_rewards)
+
+
+def run(episodes,epsilons):
+	average_rewards_for_epsilons = []
+	for epsilon in range(len(epsilons)):
+		average_rewards = train_epsilon_greedy_agent(episodes,epsilon)[0]
+		average_rewards_for_epsilons.append(average_rewards)
+	return(np.array(average_rewards_for_epsilons))	
+
 
 
 epsilons = [0, 0.01, 0.5, 0.1]
 episodes = 1000
+num_runs = 10
+
+avg_rewards_for_epsilons_num_runs = []
+for i in range(num_runs):
+	avg_reward_for_epsilons = run(episodes,epsilons)
+	avg_rewards_for_epsilons_num_runs.append(avg_reward_for_epsilons)
+
+avg_rewards_for_epsilons_num_runs = np.array(avg_rewards_for_epsilons_num_runs)
+avg_of_avg_rewards_for_epsilons = np.mean(avg_rewards_for_epsilons_num_runs, axis=0)
+
+
 plt.xlabel("Episodes")
 plt.ylabel("Average Reward")
-for epsilon in range(len(epsilons)):
-	average_rewards = train_epsilon_greedy_agent(episodes,epsilon)
-	plt.plot(average_rewards[1:], label="epsilon = "+str(epsilons[epsilon]))
-plt.legend(loc="upper right")
-plt.show()
 
+for i in range(len(avg_of_avg_rewards_for_epsilons)):
+	plt.plot(avg_of_avg_rewards_for_epsilons[i], label="epsilon = "+str(epsilons[i]))
+	plt.legend(loc="upper right")
+plt.show()
 
 
 
